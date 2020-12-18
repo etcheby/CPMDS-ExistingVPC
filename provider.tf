829e836341ad4c3bce42285c494df176c7a5a872
Eugene Tcheby <68718382+etcheby@users.noreply.github.com>
2020-12-18 17:29:38 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region                  = "us-east-1"
   shared_credentials_file = "/Users/etcheby/.aws/credentials.txt"
   profile                 = "Terraform"
-  version                 = "~> 3.15"
+  version                 = "3.22"
 }
